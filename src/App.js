44e6ea2417487e1ff1dc8e14664e5683ea7e3c90
lisset-orozco <liss612@gmail.com>
2019-07-12 17:59:36 -0500

    Connect the app with the Store through the Provider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
+import Store from './Store';
 
 const App = () => (
-  <main>
-    <h1>Football Team Manager App</h1>
-  </main>
+  <Provider store={Store} >
+    <main>
+      <h1>Football Team Manager App</h1>
+    </main>
+  </Provider>
 )
 
 export default App;
