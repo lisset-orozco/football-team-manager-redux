e38ef1d8a3e4e7888e785aad67e1fdaa6d8738d4
lisset-orozco <liss612@gmail.com>
2019-07-12 18:33:36 -0500

    Basic structure

---
+++ src/components/Players.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Players = () => (
+  <section>
+    <h2>Players</h2>
+    <div className="players-container">
+      <article>
+        <img src="" alt=""/>
+        <h3></h3>
+        <div>
+          <button></button>
+          <button></button>
+        </div>
+      </article>
+    </div>
+  </section>
+);
+
+export default Players;
