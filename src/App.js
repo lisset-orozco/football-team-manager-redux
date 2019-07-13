7e849f0d1df4ba63c28139859e2f613874a9e217
lisset-orozco <liss612@gmail.com>
2019-07-13 00:10:51 -0500

    Add component to App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Provider } from 'react-redux';
 
 import Store from './Store';
 import Players from './components/Players';
+import SelectedTeam from './components/SelectedTeam';
 
 const App = () => (
   <Provider store={Store} >
     <main>
       <h1>Football Team Manager</h1>
       <Players/>
+      <SelectedTeam/>
     </main>
   </Provider>
 )
---
+++ src/components/SelectedTeam.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const SelectedTeam = () => (
+  <section>
+  </section>
+);
+
+export default SelectedTeam;
