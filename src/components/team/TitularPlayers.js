f736ab7ac71fb7c6c07b822024368c6eacaa75a5
lisset-orozco <liss612@gmail.com>
2019-07-13 00:32:34 -0500

    Define structure and connect to the Store

--- src/components/team/TitularPlayers.js
+++ src/components/team/TitularPlayers.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const TitularPlayers = () => (
-  <section></section>
+const TitularPlayers = ({ titular_players }) => (
+  <section>
+    <h2>Titular Players</h2>
+    <div className="football-field">
+    {
+      titular_players.map(player => (
+        <article className='titular_player' key={player.id}>
+          <div>
+            <img src={player.photo} alt={player.name}/>
+            <button>X</button>
+          </div>
+          <p>{player.name}</p>
+        </article>
+      ))
+    }
+    </div>
+  </section>
 );
 
-export default TitularPlayers;
+const mapStateToProps = state => ({
+  titular_players: state.titular_players
+});
+
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TitularPlayers);
