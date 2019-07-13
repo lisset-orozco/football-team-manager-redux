2437c0727c8b94ec27b8f6b132930e79902de538
lisset-orozco <liss612@gmail.com>
2019-07-13 00:56:07 -0500

    Define structure and connect to the Store

--- src/components/team/SubstitutePlayers.js
+++ src/components/team/SubstitutePlayers.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const SubstitutePlayers = () => (
-  <section></section>
+const SubstitutePlayers = ({ substitute_players }) => (
+  <section>
+    <h2>Substitute Players</h2>
+    <div className="football-field">
+    {
+      substitute_players.map(player => (
+        <article className='substitute_player' key={player.id}>
+          <div>
+            <img src={player.photo} alt={player.name}/>
+            <button>X</button>
+          </div>
+          <p>{player.name}</p>
+        </article>
+      ))
+    }
+    </div>
+  </section>
 );
 
-export default SubstitutePlayers;
+const mapStateToProps = state => ({
+  substitute_players: state.substitute_players
+});
+
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SubstitutePlayers);
+
