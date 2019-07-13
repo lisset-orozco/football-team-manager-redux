904041015cde32aa47881fd9bf305383689ee93a
lisset-orozco <liss612@gmail.com>
2019-07-13 00:18:55 -0500

    Child components

--- src/components/SelectedTeam.js
+++ src/components/SelectedTeam.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import TitularPlayers from './team/TitularPlayers';
+import SubstitutePlayers from './team/SubstitutePlayers';
+
 const SelectedTeam = () => (
   <section>
+    <TitularPlayers />
+    <SubstitutePlayers />
   </section>
 );
 
---
+++ src/components/team/SubstitutePlayers.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SubstitutePlayers = () => (
+  <section></section>
+);
+
+export default SubstitutePlayers;
